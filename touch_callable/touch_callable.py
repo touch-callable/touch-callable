40a5fb1a784460975e6cef09e239280c8c9d7e74
pengwk2 <pengwk2@gmail.com>
2019-12-15 16:09:15 +0800

    Optimize terminal log implementation.

--- touch_callable/touch_callable.py
+++ touch_callable/touch_callable.py
@@ -13,12 +13,10 @@ import time
 import io
 
 from flask import Flask, jsonify, request, send_file, send_from_directory
+from Crypto.PublicKey.RSA import importKey
 
 
 app = Flask(__name__)
-
-werkzeug_loger = logging.getLogger("werkzeug")
-werkzeug_loger.setLevel(logging.ERROR)
 
 
 CALLABLES = None
@@ -330,6 +328,7 @@ def watch_module():
 
 def main():
     global MODULE, MODULE_PATH, KEEP_WATCHING
+
     parser = argparse.ArgumentParser(description="Touch Callable")
     parser.add_argument("module_path", type=str)
     parser.add_argument("--host", type=str, default="127.0.0.1")
@@ -340,11 +339,20 @@ def main():
     MODULE_PATH = args.module_path
     MODULE = load_module_by_path(args.module_path)
 
+    if not args.debug:
+        werkzeug_loger = logging.getLogger("werkzeug")
+        werkzeug_loger.setLevel(logging.ERROR)
+
+        os.environ["WERKZEUG_RUN_MAIN"] = "true"
+
     import threading
 
     t = threading.Thread(target=watch_module)
     t.start()
 
+    import click
+
+    click.echo(" * touch-callable serving http://{}:{}".format(args.host, args.port))
     app.run(host=args.host, debug=args.debug, port=args.port)
     KEEP_WATCHING = False
     t.join()
